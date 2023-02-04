7ca87e303d2ee97c78340f22ad6c2c94fd983821
오유진 <102959835+0hyujin@users.noreply.github.com>
2023-02-04 09:08:02 +0900

    3주차

---
+++ 3주차/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.week3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var app_name = resources.getString(R.string.app_name)
+    }
+}
